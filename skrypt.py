1897c7840992e4bf86fbac2215bba9c630b1c805
Drozyn <30470206+Drozyn@users.noreply.github.com>
2017-08-18 18:06:06 +0200

    Create skrypt.py

---
+++ skrypt.py
@@ -0,0 +1,28 @@
+# Skrypt
+# -*-coding:utf-8 -*-
+
+import urllib2
+while True:
+    adres = raw_input('Podaj url: ')
+    if not adres:
+        print "Help?"
+    else:
+        con = urllib2.urlopen(adres)
+        dane = con.read()
+        str = 'size'
+        str2 = '}'
+        index = dane.find(str)
+        index2 = dane.find(str2)
+        if index2 - index == 9:
+            rozmiar = int(dane[index + 7])
+        if index2-index == 10:
+            rozmiar =int(dane[index+7])*10 + int(dane[index+8])
+
+
+        if rozmiar < 2:
+            print "CRITICAL"
+        elif rozmiar >= 2 and rozmiar<4:
+            print "WARNING"
+        else:
+            print "OK"
+        break
